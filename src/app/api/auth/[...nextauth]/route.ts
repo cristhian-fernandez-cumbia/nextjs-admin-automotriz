1c54600ae5a9ebc23772513e0a73aa21b7b5a66f
cfernandez29 <165212076+fernandezcobraperu@users.noreply.github.com>
2024-08-17 03:42:34 -0500

    corregir variables

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import conn from '@/libs/mysql';
 import bcrypt from 'bcryptjs';
-import { User } from '@/interface/login'
+import { User } from '@/interface/login';
 
 const authOptions = {
   providers: [
@@ -19,16 +19,21 @@ const authOptions = {
 
         try {
           console.log('credentials:::', credentials);
+
           const results = await conn.query(
             'SELECT * FROM user WHERE email = ?',
             [credentials.username]
           );
+
           console.log('results:::', results);
-          if (Array.isArray(results)) {
-            const userFound = results[0] as User; 
+
+          if (Array.isArray(results) && results.length > 0) {
+            const userFound = results[0] as User;
+
             if (!userFound) {
               throw new Error('Usuario no encontrado');
             }
+
             const isValidPassword = bcrypt.compareSync(credentials.password, userFound.password);
             if (!isValidPassword) {
               throw new Error('Contraseña incorrecta');
@@ -40,7 +45,7 @@ const authOptions = {
               email: userFound.email || null
             };
           } else {
-            throw new Error('Error al obtener datos del usuario');
+            throw new Error('Usuario no encontrado');
           }
         } catch (error) {
           if (error instanceof Error) {
@@ -54,7 +59,7 @@ const authOptions = {
   ],
   pages: {
     signIn: "/auth/login",
-    error: "/auth/error",
+    error: "/auth/error"
   },
   secret: process.env.NEXTAUTH_SECRET
 };

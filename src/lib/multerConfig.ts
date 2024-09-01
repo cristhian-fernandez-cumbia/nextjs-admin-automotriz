8eae4d4b33ea75bced11d45b5ad2b8ae8819ac5f
cfernandez29 <165212076+fernandezcobraperu@users.noreply.github.com>
2024-09-01 04:16:55 -0500

    eliminar multer

--- src/lib/multerConfig.ts
+++
@@ -1,8 +0,0 @@
-import multer from 'multer';
-
-// Configura multer para almacenar archivos en memoria
-const storage = multer.memoryStorage();
-
-const upload = multer({ storage });
-
-export default upload;
